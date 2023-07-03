7b85e78e402695c1dbea99158a97c7339e8ce229
My-cheezy23 <wanjamaina8@gmail.com>
2023-07-03 11:39:52 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- * print_chessboard - Entry point
- * @a: array
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
  * Return: Always 0 (success)
  */
-void print_chessboard(char (*a)[8])
+char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
+	int k;
 
-	for (i = 0; i < 8; i++)
+	while (*s)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
 	}
+	return ('\0');
 }
