0eabefda0c7b9e907411925a3df3d50c1f57ad70
My-cheezy23 <wanjamaina8@gamil.com>
2023-08-01 12:22:30 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,6 +17,9 @@ size_t  looped_listint_len(const listint_t *head)
 	const listint_t *bird, *hare;
 	size_t nodes = 1;
 
+	if (head == NULL || head->next == NULL)
+		return (0);
+
 	bird = head->next;
 	hare = (head->next)->next;
 
